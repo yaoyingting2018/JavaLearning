fb1b66b93a2a8e9f44473f044236adab411449ab
yaoyingting <yaoyingting2013@sina.cn>
2019-06-20 14:55:39 +0800

    四种循环方式实现打印1-100

---
+++ JavaSE-day02/src/day02/array.java
@@ -0,0 +1,29 @@
+package day02;
+
+public class array {
+	public static void main(String[] args) {
+		/*boolean a=true;
+		int i=0;
+		while (a) {
+			System.out.println(i++);
+			if(i==101){
+				a=false;
+			}
+		}*/
+		int i=0;
+		/*while(i<101){
+			System.out.println(i);
+			i++;
+		}*/
+		/*while (true) {
+			System.out.println(i);
+			i+=1;
+			if(i>100)
+				break;
+		}*/
+		for(;i<100;i++){
+			System.out.println(i);
+		}
+	}
+
+}
