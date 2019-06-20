0177e885de9db950ddb400e936dfe01153c1611c
yaoyingting <yaoyingting2013@sina.cn>
2019-06-20 15:56:15 +0800

    循环遍历数组

--- JavaSE-day02/src/day02/array.java
+++ JavaSE-day02/src/day02/array.java
@@ -4,26 +4,28 @@ import java.util.Scanner;
 
 public class array {
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		// 数组变量定义,下面的语句就定义了一个用来装float数据的数组，长度为5
-	    float[] scoreArr = new float[5];
-	    int i;
-	    for(i=0;i<5;i++) {
-			System.out.println("请输入成绩：");
-			String line = sc.nextLine();
-			// 将一个数据装入数组中指定的格子（第几个索引位置）
-			scoreArr[i] = Float.parseFloat(line);
-			
+		// 造数据
+		int[] arr = new int[21];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = i;
 		}
-		
-		// 计算总成绩
-		float sum = 0;
-		for(i=0;i<5;i++) {
-			sum += scoreArr[i];
+      
+		// 打印数组中的数据
+		for (int i = arr.length - 1; i >= 0; i--) {
+			System.out.println(arr[i]);
 		}
-		
-		System.out.println("总成绩为：" + sum);
-		System.out.println("平均成绩为：" + sum/5);		
-		
+		System.out.println("----------------------------------------------");
+		int i = 0;
+		while (i < arr.length) {
+			System.out.println(arr[i]);
+			i++;
+		}
+		System.out.println("-----------------------------------------------");
+		i = 9;
+		while (i > -1) {
+			System.out.println(arr[i]);
+			i--;
+		}
+
 	}
 }
