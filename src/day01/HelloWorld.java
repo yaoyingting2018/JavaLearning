84eab9c8e9f107029b07bbdcf29f91bd67ed609c
yaoyingting <yaoyingting2013@sina.cn>
2019-06-20 11:17:32 +0800

    Scanner用法和字符串拼接

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -1,8 +1,16 @@
 package day01;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("helloWorld");
+		//获得工具类的一个对象
+		Scanner sc=new Scanner(System.in);
+		//调用对象的nextLine方法
+		String name=sc.nextLine();
+		//字符串拼接
+		String res=name+"真美,"+name+"人见人爱！";
+		System.out.println(res);
 	}
 
 }
