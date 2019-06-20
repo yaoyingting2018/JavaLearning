32edcaf19f758ebbd95352ebc56b45da2ace8f2e
yaoyingting <yaoyingting2013@sina.cn>
2019-06-20 11:56:04 +0800

    if elseif用法

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -7,25 +7,14 @@ public class HelloWorld {
 		// 首先，创建出一个用于获取终端输入的工具Scanner
 		Scanner sc=new Scanner(System.in);
 		// 先想办法获取用户输入的第一个数字
-		String number1=sc.nextLine();
-		// 利用另一个工具，将阿拉伯数字字符串 转成数学意义上的整数
-		int nb1=Integer.parseInt(number1);
-		
-		String number2=sc.nextLine();
-		int nb2=Integer.parseInt(number2);
-		
-		int add=nb1+nb2;
-		int subtract=nb1-nb2;
-		int multiply=nb1*nb2;
-		 // float这种类型可以表示小数，精度比整数要高,小数点后8位
-		float divide=(float)nb1/nb2;
-		//小数点后16位
-		double divide1=(double)nb1/nb2;
-		System.out.println("相加的结果为："+add);
-		System.out.println("相减的结果为："+subtract);
-		System.out.println("相乘的结果为："+multiply);
-		System.out.println("相除的结果为："+divide);
-		System.out.println("相除的结果为："+divide1);
+		String name=sc.nextLine();
+		//"Tom".equals(name)
+		if(name.equals("Tom")){
+			System.out.println("欢迎您,tom");
+		}else if (name.equals("Jim")) {
+			System.out.println("你好，Jim");			
+		}else {
+			System.out.println(name+"滚蛋，你在本系统没有权限");
+		}		
 	}
-
 }
