48e039702d8a8bc1e726203942458da5ab901d9c
yaoyingting <yaoyingting2013@sina.cn>
2019-06-20 16:13:12 +0800

    while循环打印100以内奇数

---
+++ JavaSE-day02/src/day02/Circle.java
@@ -0,0 +1,20 @@
+package day02;
+
+public class Circle {
+	public static void main(String[] args) {
+		int i=1;
+		while(i<100){
+			System.out.println(i);
+			i+=2;
+		}
+		/*int i=0;
+		while(i<100){
+			if(i%2==0){}else{
+				System.out.println(i);
+			}			
+			i++;
+		}*/
+		
+	}
+
+}
