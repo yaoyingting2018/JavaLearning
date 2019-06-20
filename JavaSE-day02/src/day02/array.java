2f8f55f227c31e952d4a3f7e0197ef72aded0fd8
yaoyingting <yaoyingting2013@sina.cn>
2019-06-20 15:58:20 +0800

    求数组的最大值和最小值

--- JavaSE-day02/src/day02/array.java
+++ JavaSE-day02/src/day02/array.java
@@ -5,27 +5,31 @@ import java.util.Scanner;
 public class array {
 	public static void main(String[] args) {
 		// 造数据
-		int[] arr = new int[21];
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = i;
-		}
-      
-		// 打印数组中的数据
-		for (int i = arr.length - 1; i >= 0; i--) {
-			System.out.println(arr[i]);
-		}
-		System.out.println("----------------------------------------------");
-		int i = 0;
-		while (i < arr.length) {
-			System.out.println(arr[i]);
-			i++;
-		}
-		System.out.println("-----------------------------------------------");
-		i = 9;
-		while (i > -1) {
-			System.out.println(arr[i]);
-			i--;
+		int[] arr = new int[5];
+		arr[0] = 10;
+		arr[1] = 20;
+		arr[2] = 25;
+		arr[3] = 30;
+		arr[4] = 18;
+
+		// 实现需求
+		// 需求1： 求最大值
+		int tmp = arr[0];
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] > tmp) {
+				tmp = arr[i];
+			}
 		}
 
+		System.out.println("最大值为： " + tmp);
+
+		tmp = arr[0];
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] < tmp) {
+				tmp = arr[i];
+			}
+		}
+
+		System.out.println("最小值为： " + tmp);
 	}
 }
