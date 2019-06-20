d2fa21830c8bbbc3864e22397f5ff2c2563ff0b6
yaoyingting <yaoyingting2013@sina.cn>
2019-06-20 15:25:39 +0800

    数组的定义使用

--- JavaSE-day02/src/day02/array.java
+++ JavaSE-day02/src/day02/array.java
@@ -1,29 +1,29 @@
 package day02;
 
+import java.util.Scanner;
+
 public class array {
 	public static void main(String[] args) {
-		/*boolean a=true;
-		int i=0;
-		while (a) {
-			System.out.println(i++);
-			if(i==101){
-				a=false;
-			}
-		}*/
-		int i=0;
-		/*while(i<101){
-			System.out.println(i);
-			i++;
-		}*/
-		/*while (true) {
-			System.out.println(i);
-			i+=1;
-			if(i>100)
-				break;
-		}*/
-		for(;i<100;i++){
-			System.out.println(i);
+		Scanner sc=new Scanner(System.in);
+		// 数组变量定义,下面的语句就定义了一个用来装float数据的数组，长度为5
+	    float[] scoreArr = new float[5];
+	    int i;
+	    for(i=0;i<5;i++) {
+			System.out.println("请输入成绩：");
+			String line = sc.nextLine();
+			// 将一个数据装入数组中指定的格子（第几个索引位置）
+			scoreArr[i] = Float.parseFloat(line);
+			
 		}
+		
+		// 计算总成绩
+		float sum = 0;
+		for(i=0;i<5;i++) {
+			sum += scoreArr[i];
+		}
+		
+		System.out.println("总成绩为：" + sum);
+		System.out.println("平均成绩为：" + sum/5);		
+		
 	}
-
 }
