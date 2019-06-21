2bfb3d71157c1edbc20c562f6bcd78ed7f8a6c63
yaoyingting <yaoyingting2013@sina.cn>
2019-06-21 13:36:26 +0800

    list的get,remove.set用法

--- JavaSE-day04/src/day04/ListDemo.java
+++ JavaSE-day04/src/day04/ListDemo.java
@@ -9,20 +9,23 @@ public class ListDemo {
 		list.add(10);
 		list.add(20);
 		list.add(18);
-		ListUtils.sort(list);
-		System.out.println(list);
-
-		int max = ListUtils.getMax(list);
-		System.out.println("最大值为：" + max);
-
-		// 测--求最小值
-		int min = ListUtils.getMin(list);
-		System.out.println("最小值为：" + min);
-
-		// 测--求平均值
-		float avg = ListUtils.getAvg(list);
-		System.out.println("平均值为：" + avg);
-
+		// 从arraylist中读取数据
+		int a=list.get(0);
+		System.out.println(a);		
+		System.out.println("------------------------");
+		// 遍历list： 将整个arraylist的数据按脚标顺序全部打印出来
+		for(int i=0;i<list.size();i++){
+			System.out.println(list.get(i));
+		}
+		System.out.println("------------------------");
+		// 从list中移除数据
+		list.remove(1);
+		for(int i=0;i<list.size();i++){
+			System.out.println(list.get(i));
+		}
+		
+		list.set(2, 34);
+		System.out.println(list.get(2));
 	}
 
 }
